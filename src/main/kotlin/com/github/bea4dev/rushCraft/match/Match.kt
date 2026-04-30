b96dcd734d1b692b32d827eedae0a3b278c7a7a1
bea4dev <bea0224@outlook.jp>
2026-04-30 16:34:24 +0900

    commit

--- src/main/kotlin/com/github/bea4dev/rushCraft/match/Match.kt
+++ src/main/kotlin/com/github/bea4dev/rushCraft/match/Match.kt
@@ -1,11 +1,16 @@
 package com.github.bea4dev.rushCraft.match
 
 import com.github.bea4dev.rushCraft.scheduler.Tickable
+import net.kyori.adventure.sound.Sound
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.title.Title
 import org.bukkit.entity.Player
 import java.util.stream.Collectors
 
 class Match<G : MatchGameMode<G>>(private val mode: G, val teams: List<MatchTeam>) : Tickable {
     private val objective: MatchObjective<G> = mode.createObjective()
+    private var isRunning = true
 
     fun getMembers(): List<Player> {
         return teams.stream()
@@ -13,11 +18,49 @@ class Match<G : MatchGameMode<G>>(private val mode: G, val teams: List<MatchTeam
             .collect(Collectors.toList())
     }
 
-    override fun tick() {
+    private fun win(winners: List<MatchTeam>) {
+        val isDraw = winners.size == teams.size
+        val winnerMembers = teams.stream()
+            .flatMap { team -> team.getMembers().stream() }
+            .collect(Collectors.toList())
 
+        when (isDraw) {
+            true -> {
+                winnerMembers.forEach {
+                    it.sendMessage(Component.translatable("rushcraft.match.finished.draw").color(NamedTextColor.GREEN))
+                    it.showTitle(
+                        Title.title(
+                            Component.translatable("rushcraft.match.finished.draw").color(NamedTextColor.GREEN),
+                            Component.empty()
+                        )
+                    )
+                }
+            }
+
+            false -> {
+
+            }
+        }
+
+        winnerMembers.forEach {
+            it.playSound(
+                Sound.sound(org.bukkit.Sound.ENTITY_PLAYER_LEVELUP, Sound.Source.MASTER, 1f, 1f),
+                Sound.Emitter.self()
+            )
+        }
+    }
+
+    override fun tick() {
+        val winners = objective.getWinners(this)
+
+        if (winners.isNotEmpty()) {
+            this.win(winners)
+            this.isRunning = false
+            return
+        }
     }
 
     override fun isAlive(): Boolean {
-        return this.objective.getWinners(this).isEmpty()
+        return this.isRunning
     }
 }
